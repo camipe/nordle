baa665254b879d8d981ad603e0ff29370f3d1363
Micael Persson <micael.persson@knowit.se>
2024-10-01 09:26:44 +0200

    Add Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <h1 class="text-2xl text-center">
+      Tailwind test
+    </h1>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
-  modules: ['@nuxtjs/supabase']
+  // modules: ['@nuxtjs/supabase'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
