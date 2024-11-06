bffc945524bb36644a45330c1a7d15cb2a11e632
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-11-06 15:49:22 +0100

    init login page

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-2xl text-center">
+        Coming soon ⚡️🤖
+      </h1>
+      <p>IT WILL BE COOL</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-2xl text-center">
+      Login Page
+    </h1>
+  </div>
+</template>
