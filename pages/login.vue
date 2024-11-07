bb2a02c1715d799011ee13a7e630a26d02fbb3a5
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-11-07 07:22:00 +0100

    working on login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-2xl text-center">
-      Login Page
-    </h1>
+  <div class="flex flex-col items-center justify-center h-screen bg-knowit-black text-knowit-white">
+    <div class="mb-12">
+      <h1 class="text-2xl text-center">Nördle</h1>
+      <p class="text-purple-100">Your daily nörd game by Knowit</p>
+    </div>
+
+    <div class="flex flex-col items-start mt-12 space-y-4">
+      <button @click="login" class="w-64 px-4 py-4 border rounded-xl">Logga in med Google</button>
+      <button @click="login" class="w-64 px-4 py-4 border rounded-xl">Logga in med Apple</button>
+      <button @click="login" class="w-64 px-4 py-4 border rounded-xl">Logga in med GitHub</button>
+    </div>
   </div>
 </template>
