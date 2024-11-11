4ebc7b902886ff2f41264982abf1b54d9df1e491
Dennis Kjellin <dennis.kjellin@knowit.se>
2024-11-11 16:04:49 +0100

    created a navbar component, created new page for "hantering av personuppgifter", removed the darkMode class - it made the dark prefix not work.

---
+++ components/NavBarBottom.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="fixed bottom-0 left-0 right-0 dark:text-knowit-white">
+    <nav aria-label="Huvudnavigering">
+      <ul class="flex justify-around py-3 space-x-6 sm:space-x-4">
+        <!-- Start Link ( change to  NuxtLink later ) -->
+        <li>
+          <a href="#start" aria-label="Gå till Start">
+            <IconsHome />
+            <span class="text-sm">Start</span>
+          </a>
+        </li>
+
+        <!-- Topplistan Link ( change to  NuxtLink later )-->
+        <li>
+          <a href="#topplistan" aria-label="Gå till Topplistan">
+            <IconsTrophy />
+            <span class="text-sm">Topplistan</span>
+          </a>
+        </li>
+
+        <!-- Instruktioner Link ( change to  NuxtLink later ) -->
+        <li>
+          <a href="#instruktioner" aria-label="Gå till Instruktioner">
+            <IconsQuestion />
+            <span class="text-sm">Instruktioner</span>
+          </a>
+        </li>
+
+        <!-- Min Profil Link -->
+        <li>
+          <NuxtLink to="hantering-av-personuppgifter" aria-label="Gå till Min Profil">
+            <IconsUser />
+            <span class="text-sm">Min profil</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+a {
+  @apply flex flex-col items-center text-center hover:text-blue-80 dark:hover:text-purple-100;
+}
+
+</style>
---
+++ pages/hantering-av-personuppgifter.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="flex flex-col h-screen px-6 bg-knowit-white dark:bg-knowit-black dark:text-knowit-white">
+    <section class="max-w-[1000px] w-full mx-auto">
+      <header class="mb-6">
+        <h1 class="text-4xl font-semibold">Hantering av personuppgifter</h1>
+      </header>
+
+      <div class="space-y-6">
+        <p>Vi värnar om din personliga integritet och tar frågor om dataskydd på största allvar.</p>
+
+        <p>
+          I vår integritetspolicy beskriver vi den behandling av personuppgifter som sker inom ramen för vår verksamhet, ändamålen med behandlingen,
+          vilket lagstöd vi har för behandlingen, vilka vi kan komma att dela dina personuppgifter med och dina rättigheter i förhållande till dina
+          personuppgifter. För att du ska förstå hur dina personuppgifter behandlas rekommenderar vi att du läser vår Integritetspolicy noggrant.
+        </p>
+
+        <a href="#" class="underline dark:text-purple-100 text-blue-80">
+          Ladda hem och läs vår Integritetspolicy
+        </a>
+
+        <p>
+          För frågor kring vår Integritetspolicy och för att göra gällande de rättigheter du har beträffande behandlingen av dina personuppgifter är
+          du alltid välkommen att kontakta oss på
+        </p>
+
+        <a href="mailto:dataprotection@knowitgroup.com" class="underline dark:text-purple-100 text-blue-80" aria-label="Contact us via email">
+          dataprotection@knowitgroup.com
+        </a>
+      </div>
+    </section>
+  </article>
+
+  <NavBarBottom />
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
