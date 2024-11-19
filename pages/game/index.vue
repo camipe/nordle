c51d95c445fddcdbb424f4c4990e60e2d39c21fa
Emma Thiebaut <Emma.Thiebaut@knowit.se>
2024-11-19 09:50:05 +0100

    Added new nav component

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -95,25 +95,8 @@ onMounted(() => {
       <p>Nästa spel om: 13:34 tim</p>
     </div>
     <Keyboard :letters="letters" :finished="finished" :class="finishedClass" @submit="submit" @undo="undo" @key-click="keyClick" />
-    <div class="flex flex-col w-screen px-4 pt-4 pb-8">
-      <div class="grid grid-cols-4 gap-2 text-center grow content-stretch">
-        <button>
-          <IconsHome />
-          Start
-        </button>
-        <button>
-          <IconsTrophy />
-          Topplistan
-        </button>
-        <button>
-          <IconsQuestion />
-          Instruktioner
-        </button>
-        <button>
-          <IconsUser />
-          Min profil
-        </button>
-      </div>
+    <div class="flex flex-col w-screen px-4 py-8">
+      <NavBarBottom />
     </div>
   </div>
 </template>
